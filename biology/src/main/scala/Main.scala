864ef6814bbc6431a3ba37284528c6874264c56d
RedCrabVb <cany245@mail.ru>
2021-12-14 14:42:11 +0300

    Add iteration

--- biology/src/main/scala/Main.scala
+++ biology/src/main/scala/Main.scala
@@ -10,10 +10,28 @@ object Main extends App {
     if ((q && !p) || (q ^ r)) 1 else 0
   }
 
-  val x = (3, 5, 3)
+  def iteration(str: Array[Int], i: Int = 0): Array[Int] = {
+    def tryVal(index: Int): Option[Int] =
+      try {
+        Option(str(index))
+      } catch {
+        case e: IndexOutOfBoundsException => None
+      }
+    val result = rule110(str(i), tryVal(i + 1).getOrElse(0), tryVal(i + 2).getOrElse(0))
+    str(i) = result
+    if (i < str.length - 1) {
+      iteration(str, i + 1)
+    } else {
+      str
+    }
+  }
 
-  println(rule110(1, 1, 1))
-  println(rule110(1, 1, 1))
+  var result = Array(0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0)
+  (0 to 100).map(i => { result = iteration(result); println((for (x <- result) yield {x match {
+    case 0 => " "
+    case 1 => "*"
+    case _ => None
+  }}).mkString(""))})
 
-  println()
+
 }
