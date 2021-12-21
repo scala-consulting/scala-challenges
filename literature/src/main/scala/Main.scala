aabd032479d7de6023759e54883dc322e96b2383
RedCrabVb <cany245@mail.ru>
2021-12-21 17:10:32 +0300

    Fix literature

--- literature/src/main/scala/Main.scala
+++ literature/src/main/scala/Main.scala
@@ -2,13 +2,14 @@ import java.time.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 import java.util.concurrent.Executors
 import scala.io.{BufferedSource, Source}
+import scala.sys.exit
 import scala.util.{Failure, Success}
 //https://raw.githubusercontent.com/benschw/shakespeare-txt/master/shakespeare-hamlet-25.txt
 
 object Main {
   given ExecutionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
 
-  def avgStrInText(text: String): Double = {
+  def avgStrInText(text: String): Float = {
     val lengthStr = text.split(" ").map(_.length)
 
     (lengthStr.sum.toFloat / lengthStr.length)
@@ -16,9 +17,14 @@ object Main {
 
 
   def main(args: Array[String]): Unit = {
-    val text = Future[String] { Source.fromURL(args(0)).getLines().mkString(" ") }
+    val text = Future[String] {
+      Source.fromURL(args(0)).getLines().mkString(" ")
+    }
     text.onComplete {
-      case Success(posts) => println(avgStrInText(posts))
+      case Success(text) => {
+        printf("%.1f\n", avgStrInText(text))
+        exit(0)
+      }
       case Failure(t) => t.printStackTrace()
     }
   }
