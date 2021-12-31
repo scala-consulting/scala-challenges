6e3ecd15ec83cb3406e742abd543e186f8bfe926
RedCrabVb <cany245@mail.ru>
2021-12-31 10:21:06 +0300

    Fix task

--- literature/build.sbt
+++ literature/build.sbt
@@ -3,3 +3,5 @@ name := "literature"
 version := "0.1"
 
 scalaVersion := "3.1.0"
+
+libraryDependencies += "org.typelevel" %% "cats-effect" % "2.5.3"
--- literature/src/main/scala/Main.scala
+++ literature/src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import cats.effect.IO
+
 import java.time.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 import java.util.concurrent.Executors
@@ -7,25 +9,26 @@ import scala.util.{Failure, Success}
 //https://raw.githubusercontent.com/benschw/shakespeare-txt/master/shakespeare-hamlet-25.txt
 
 object Main {
-  given ExecutionContext = ExecutionContext.fromExecutor(Executors.newCachedThreadPool())
-
-  def avgStrInText(text: String): Float = {
-    val lengthStr = text.split(" ").map(_.length)
-
-    (lengthStr.sum.toFloat / lengthStr.length)
-  }
-
-
   def main(args: Array[String]): Unit = {
-    val text = Future[String] {
-      Source.fromURL(args(0)).getLines().mkString(" ")
+    def countWorld(words: String) = IO {
+      words.split(" ").length
     }
-    text.onComplete {
-      case Success(text) => {
-        printf("%.1f\n", avgStrInText(text))
-        exit(0)
-      }
-      case Failure(t) => t.printStackTrace()
+
+    def countChar(line: String) = IO {
+      line.replace(" ", "").length
     }
+
+    val avgStr = Source.fromURL(args(0)).getLines().mkString(" ").split("\n").map(s =>
+      (for {
+        x <- countWorld(s)
+        y <- countChar(s)
+      } yield {
+        y.toFloat / x.toFloat
+      }).unsafeRunSync()
+    )
+
+    val result = avgStr.sum / avgStr.length
+    printf("%.1f\n", result)
   }
+
 }
